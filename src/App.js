f7a180df1d1c6a190369f1a73af93fb1d8718a6b
haykaslanyan <hayk.aslan@mail.ru>
2021-12-15 20:35:37 +0400

    change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import AllServices from './pages/AllServices';
-import Home from './pages/Home';
+import { Route, Routes} from 'react-router-dom';
+import './App.css';import Home from './pages/Home';
+;
+
 
 function App() {
   return (
         <>
     <Routes>
-      <Route index path="/" element={<Home />}/>
       <Route path="/" element={<Home />}/>
-      <Route path="/" element={<Navbar />}/>
-      <Route path="/" element={<AllServices />}/>
     </Routes>
-    <SignUp />
-      <SignIn />
-<<<<<<< Updated upstream
-      {console.log("asdasd")}
-    </div>
-=======
-    </>
-      
->>>>>>> Stashed changes
+   </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import SignIn from '../components/SignIn'
+import SignUp from '../components/SignUp'
 
 function Home() {
     return (
         <div>
-           
+           <Navbar />
+           {/* <SignUp /> */}
+           <SignIn />
+           {/* <div>dfff</div> */}
         </div>
     )
 }
