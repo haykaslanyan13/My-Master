7445afe1d4f1fc76fc1e883f106a550b1bcc6889
gor017 <85189675+gor017@users.noreply.github.com>
2021-12-16 17:32:12 +0400

    navbar some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 function Navbar() {
+
+  const [searchText, setSearchText] = useState('')
+
   return (
     <div className="navbar">
       <div className="leftSide">
         <div className="logo">
-          <span style={{ color: "#007ce7", fontSize: "30px" }}>M.</span>
-          <span style={{ color: "#007f8b", fontSize: "30px" }}>MASTER</span>
+          <span style={{ color: "#007ce7", fontSize: "30px" }}>My </span>
+          <span style={{ color: "#007f8b", fontSize: "30px" }}>Master</span>
         </div>
         <Link to="/allservices">All Services</Link>
         <div className="search-box">
           <button className="btn-search"><FontAwesomeIcon icon={faSearch} /> </button>
-          <input type="text" className="input-search" placeholder="search..." />
+          <input type="text" className="input-search" onChange={(e) => {setSearchText(e.target.value)}} value={searchText} placeholder="The service you need..." />
         </div>
       </div>
       <div className="rightSide">
