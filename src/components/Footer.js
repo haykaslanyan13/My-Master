c9cb5ac09ee0cbe6275784efab8a7e19131452a1
unknown <prikhodachristina@gmail.com>
2021-12-20 21:14:58 +0400

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,77 @@
+import React from "react";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import "../styles/Footer.css";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  Link,
+  Typography,
+} from "@material-ui/core";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Box>
+        <Container maxWidth="lg">
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={4}>
+              <Typography style={{ color: "#3b3d3d" }}>Help</Typography>
+              <Box  pt={3}>
+                <Link href="/" style={{ color: "white" }}>
+                  Contact us
+                </Link>
+              </Box>
+              <Box  pt={3}>
+                <Link href="/" color="inherit">
+                  Support
+                </Link>
+              </Box>
+              <Box pt={3}>
+                <Link href="/" color="inherit">
+                  Privacy
+                </Link>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Typography style={{ color: "#3b3d3d" }}>Help</Typography>
+              <Box  pt={3}>
+                <Link href="/" color="inherit">
+                  Contact us
+                </Link>
+              </Box>
+              <Box pt={3}>
+                <Link href="/" color="inherit">
+                  Support
+                </Link>
+              </Box>
+              <Box pt={3}>
+                <Link href="/" color="inherit">
+                  Privacy
+                </Link>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box>
+                <Typography>
+                  Follow us! <InstagramIcon /> <TwitterIcon />{" "}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography align="center">LOGO</Typography>
+              </Box>
+            </Grid>
+          </Grid>
+          <Divider variant="fullWidth" />
+          <Typography align="center">
+            &copy; 2021 mymaster.com All Rights Reserved.
+          </Typography>
+        </Container>
+      </Box>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 import Navbar from "./Navbar"
@@ -20,6 +21,29 @@ function Main() {
       </Routes>
     </div>
   )
+=======
+import React from 'react'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import Navbar from './Navbar'
+import AllServices from '../pages/AllServices'
+import Home from '../pages/Home';
+import Footer from './Footer';
+
+
+
+function Main() {
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="allservices" element={<AllServices />} />
+                <Route path="home" element={<Home />} />
+                <Route path="*" element={ <Navigate to="home" />} />
+            </Routes>
+            <Footer />
+        </div>
+    )
+>>>>>>> Stashed changes
 }
 
 export default Main
