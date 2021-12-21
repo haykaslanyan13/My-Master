5c150eacef955c5ed4b6502caae3040ba6d52b81
Gor Khachatryan <gorkhachatryan017@gmail.com>
2021-12-21 22:38:16 +0400

    store conflicts resolved

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,17 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import UserReducer from "./UserSlice"
 
-<<<<<<< HEAD
-const store = configureStore({
- reducer: {
-  user: UserReducer
- }
-})
-export default store
-=======
 export const store = configureStore({
   reducer: {
     user: UserReducer,
   },
 })
->>>>>>> master
--- src/Redux/UserSlice.js
+++ src/Redux/UserSlice.js
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-<<<<<<< HEAD
  user: null,
  id: null,
  masterFilter: "",
@@ -20,27 +19,12 @@ const UserSlice = createSlice({
   },
   setMasterRating(state, action){
     state.masterRating = action.payload
-  }
+  },
+  setUser(state, action) {
+    state.user = action.payload
+  },
  }
 })
 
 export default UserSlice.reducer
 export const {setMasterFilter, setUser, setMasterRating} = UserSlice.actions
-=======
-  user: null,
-  id: null,
-}
-
-export const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    setUser(state, action) {
-      state.user = action.payload
-    },
-  },
-})
-
-export const { setUser } = userSlice.actions
-export default userSlice.reducer
->>>>>>> master
