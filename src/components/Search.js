eadac41b9ace998729d9f9f45a64e23ead2ea4d2
nataliekalantaryan <nataliekalantaryan7@gmail.com>
2021-12-21 16:53:04 +0400

    homePage

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,30 @@
-import { ButtonBase, Paper, styled, TextField, Typography } from "@mui/material"
-import React, { useState } from "react"
-import Grid from "@mui/material/Grid"
-import allServicesData from "../data/AllServicesData"
-import "../styles/Search.css"
+import {
+  ButtonBase,
+  Paper,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import allServicesData from "../data/AllServicesData";
+import "../styles/Search.css";
+// import { announcement } from "./images/announcement.png";
 
 const Img = styled("img")({
   margin: "auto",
   display: "block",
   maxWidth: "100%",
   maxHeight: "100%",
-})
+});
 
 function Search() {
-  const [filter, setFilter] = useState("")
+  const [filter, setFilter] = useState("");
 
   const searchText = (e) => {
-    setFilter(e.target.value)
-  }
-  let dataSearch = allServicesData.cardData.filter((item) =>
-    item.title.toLowerCase().includes(filter)
-  )
+    setFilter(e.target.value);
+  };
+  let dataSearch = allServicesData.cardData.filter( item => item.title.toLowerCase().includes(filter) )
 
   return (
     <div>
@@ -48,45 +53,50 @@ function Search() {
         /> */}
       </div>
       <Grid container>
-        {dataSearch.map((item) => {
-          return (
-            <Grid item xs={12} md={6} key={item.id}>
-              <Paper
-                sx={{
-                  p: 2,
-                  margin: "70px",
-                  maxWidth: 500,
-                  flexGrow: 1,
-                  backgroundColor: "#b2bcc0",
-                  borderRadius: "30px",
-                }}
-              >
-                <Grid container spacing={2}>
-                  <Grid item>
-                    <ButtonBase sx={{ width: 128, height: 128 }}>
-                      <Img alt="complex" src={item.img} />
-                    </ButtonBase>
-                  </Grid>
-                  <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                      <Grid item xs>
-                        <Typography gutterBottom variant="subtitle1" component="div">
-                          <h1 style={{ color: "white" }}>{item.title}</h1>
-                        </Typography>
-                        <Typography variant="body2" gutterBottom>
-                          <h4>{item.desc}</h4>
-                        </Typography>
-                      </Grid>
-                    </Grid>
+
+      {dataSearch.map((item) => {
+        return (
+          <Grid item xs= {12} md ={6} key={item.id}>
+          <Paper
+            sx={{
+               p: 2,
+               margin: "70px",
+              maxWidth: 500,
+              flexGrow: 1,
+              backgroundColor: "#b2bcc0",
+              borderRadius: "30px",
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item>
+                <ButtonBase sx={{ width: 128, height: 128 }}>
+                  <Img alt="complex" src={item.img} />
+                </ButtonBase>
+              </Grid>
+              <Grid item xs={12} sm container>
+                <Grid item xs container direction="column" spacing={2}>
+                  <Grid item xs>
+                    <Typography
+                      gutterBottom
+                      variant="subtitle1"
+                      component="div"
+                    >
+                      <h1 style={{color: "white"}} >{item.title}</h1>
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                      <h4>{item.desc}</h4>
+                    </Typography>
                   </Grid>
                 </Grid>
-              </Paper>
+              </Grid>
             </Grid>
-          )
-        })}
+          </Paper>
+          </Grid>
+        );
+      })}
       </Grid>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
