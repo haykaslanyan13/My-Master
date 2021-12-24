f9bca1b5721143e27c80f89ca9593be832ff9afc
haykaslanyan <hayk.aslan@mail.ru>
2021-12-24 23:06:24 +0400

    somechanges

--- src/Redux/UserSlice.js
+++ src/Redux/UserSlice.js
@@ -1,30 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
- user: null,
- id: null,
- masterFilter: "",
- masterRating: []
-}
+  user: null,
+  isAuthenticating: true,
+  id: null,
+  masterFilter: "",
+  masterRating: [],
+};
 
 const UserSlice = createSlice({
- name: 'user',
- initialState,
- reducers: {
-  setMasterFilter(state, action){
-    state.masterFilter = action.payload
+  name: "user",
+  initialState,
+  reducers: {
+    setMasterFilter(state, action) {
+      state.masterFilter = action.payload;
+    },
+    setUser(state, action) {
+      state.user = action.payload;
+      state.isAuthenticating = false;
+    },
+    setMasterRating(state, action) {
+      state.masterRating = action.payload;
+    },
   },
-  setUser(state, action){
-    state.user = action.payload
-  },
-  setMasterRating(state, action){
-    state.masterRating = action.payload
-  },
-  setUser(state, action) {
-    state.user = action.payload
-  },
- }
-})
+});
 
-export default UserSlice.reducer
-export const {setMasterFilter, setUser, setMasterRating} = UserSlice.actions
+export default UserSlice.reducer;
+export const { setMasterFilter, setUser, setMasterRating } = UserSlice.actions;
